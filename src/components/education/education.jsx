ad0c462c66cc0562a15dcc2c9a81006044e28771
Zdeal <jedimastershake@gmail.com>
2021-09-17 19:26:38 -0400

    playing with carousel

--- src/components/education/education.jsx
+++ src/components/education/education.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./style.scss";
 import Carousel from "react-elastic-carousel";
 import "../../utils/method.jsx";
 
+const breakPoints = [
+  { width: 1, diploma: 1 },
+  { width: 550, diploma: 2 },
+  { width: 768, diploma: 3 },
+  { width: 1200, diploma: 4 },
+];
+
 function Education() {
   return (
     <div className="education" id="education">
-      education
+      <Carousel breakPoints={breakPoints}>
+        <Item>Associattes in Applied Sciences; Radiography</Item>
+        <Item>Bachelors of Science; Business Administration</Item>
+        <Item>Certificate; Coding Boot Camp</Item>
+      </Carousel>
     </div>
   );
 }
